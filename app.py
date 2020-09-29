e00f97b8e4941dd3f26409e1c37fdf7cacf98209
David LaClair <bunkerdude103@hotmail.com>
2020-09-29 10:41:02 -0400

    v3.0
    Logging
    Prod/Dev client
    80% Done with multi-guild bot

--- app.py
+++ app.py
@@ -1,77 +1,100 @@
 import configparser
 import discord
+import logging
 import os
 from games.games import Games
 
 config_file = "config.ini"
 
-print("Reading Config file")
+print("Reading config file.")
 config = configparser.ConfigParser()
 config.read_file(open(config_file))
+print("Done reading config file.")
 
-print("Initializing Discord Bot")
+prod = config["General"]["prod"].lower() in ["true", "1"]
+if prod:
+    print("Starting prod")
+    secret = config["General"]["secret"]
+    log_name = "prod.log"
+else:
+    print("Starting dev")
+    secret = config["General"]["dev_secret"]
+    log_name = "dev.log"
+
+print("Starting logger.")
+log_format = "%(asctime)s %(message)s"
+logging.basicConfig(filename=log_name, format=log_format, level=logging.INFO, force=True)
+logging.info("")
+logging.info("Logging initialized.")
+
+logging.info("Initializing Discord Bot.")
 client = discord.Client()
 
 games_name = config["Games"]["name"]
 games_category_name = config["Games"]["category_name"]
-games = Games(games_name, games_category_name)
+games = Games(None, games_name, games_category_name)
 
+def generate_help():
+    help_msg = """Hi {user.mention} I'm here to help!
 
-async def userToMember(user):
-    print("We are trying to convert {user} to a member.".format(user=user))
-    member = discord.utils.get(client.guilds[0].members, name=user.name)
+Here are available commands:
+General:
+!help                   Gets you this help message!
 
-    print("Here's the result of converting {user} to a member: {member}".format(user=user, member=member))
-    return member
+Games:
+!games                  Gives you a list of all available game channels
+!join game <name>       Join a specific game channel
+!leave game <name>      Leave a specific game channel"""
+    return help_msg
 
-async def does_member_have_role(member, role):
-    print("We are checking if '{member}' has role '{role}'".format(member=member, role=role))
+async def does_member_have_role(guild, member, role):
+    logging.info("'{guild_name}'\t{member}\t{action}".format(guild_name=guild.name, member=member, action="check_role"))
+    logging.info("'{guild_name}'\t{member}\t{action}\t{result}".format(guild_name=guild.name, member=member, action="get_member_roles", result=member.roles))
 
-    #Get user roles
-    print("Member roles:")
-    print(member.roles)
-
+    has_role = False
     #Does user roles match role to add?
-    #Return true if yes, false if no
     if role in member.roles:
-        print("They have the role!")
-        return True
+        has_role = True
     
-    print("They do not have the role!")
-    return False
+    logging.info("'{guild_name}'\t{member}\t{action}\t{result}".format(guild_name=guild.name, member=member, action="check_role", result=has_role))
+    return has_role
 
-async def join_role(member, role):
-    print("We are adding {member} to '{role}'.".format(member=member, role=role))
+async def join_role(guild, member, role):
+    logging.info("'{guild_name}'\t{member}\t{action}\t{role_name}".format(guild_name=guild.name, member=member, action="join_role", role_name=role.name))
 
+    #TODO: Add Try/Catch Block
     await member.add_roles(role)
-    print("Added {member} to '{role}'".format(member=member.name, role=role))
-    
-async def leave_role(member, role):
-    print("We are removing {member} from '{role}'.".format(member=member, role=role))
 
+    logging.info("'{guild_name}'\t{member}\t{action}\t{role_name}\t{result}".format(guild_name=guild.name, member=member, action="join_role", role_name=role.name,result="Added to role"))
+    
+async def leave_role(guild, member, role):
+    logging.info("'{guild_name}'\t{member}\t{action}\t{role_name}".format(guild_name=guild.name, member=member, action="leave_role", role_name=role.name))
+
+    #TODO: Add Try/Catch Block
     await member.remove_roles(role)
-    print("Removed {member} from '{role}'".format(member=member.name, role=role))
+
+    logging.info("'{guild_name}'\t{member}\t{action}\t{role_name}\t{result}".format(guild_name=guild.name, member=member, action="leave_role", role_name=role.name,result="Removed from role"))
 
 @client.event
 async def on_ready():
-    print("{client} has connect to Discord! Beginning initialization".format(client=client.user))
+    logging.info("'{client}' has connected to Discord.".format(client=client.user))
+
+    games.guild = client.guilds[0]
 
     #Find and set the category ID(s) for each media type
     games_category_id = discord.utils.get(client.guilds[0].categories, name=games_category_name).id
     games.set_category_id(games_category_id)
 
     #Populate the channel list for each media type
-    games.get_channels(client.guilds[0])
+    games.get_channels()
 
     #Populate the existing role list for each media type
-    games.get_roles(client.guilds[0])
+    games.get_roles()
 
     #Create any missing roles for each media type
-    games.create_missing_roles(client.guilds[0])
+    await games.create_missing_roles()
 
-    print("Done with initialization!")
-    print()
-    print()
+    ("'{client}' is done initializing and listening!".format(client=client.user))
 
 @client.event
 async def on_message(message):
@@ -83,92 +106,85 @@ async def on_message(message):
     if user == client.user:
         return
     
-    #Only listen to the bot channel for help
-    if not isinstance(channel, discord.channel.DMChannel):
-        if channel.name == "bot" and message.content == '!help':
-            print("{user} has requested help.".format(user=user))
-            helpmessage = config["General"]["helpmessage"]
+    #Mark the message as processed
+    await message.add_reaction("\N{SQUARED OK}")
+    
+    #DMs should only be requesting help
+    if isinstance(channel, discord.channel.DMChannel):
+        if message.content=="!help":
+            logging.info("{user}\t{action}\t".format(user=user, action="request_help_dm"))
+            helpmessage = generate_help()
             await message.author.send(helpmessage.format(user=user))
-            await message.delete()
-        elif channel.name == "bot":
-            await message.author.send("Please only use '!help' in the bot channel.")
-            await message.delete()
+            return
+        else:
+            logging.info("{user}\t{action}\t'{message}'".format(user=user, action="invalid_dm_request",message=message.content))
+            await message.author.send("Command not supported in DMs.")
+            return
 
-    #From this point on, only allow DMs
-    if not isinstance(channel, discord.channel.DMChannel):
-        return
-
-    #Seperate log messages
-    print()
+    #Since the message isn't a DM, get the guild from which the message came
+    guild = message.guild
 
     #Message a user with available commands
     if message.content == '!help':
-        print("{user} has requested help.".format(user=user))
-        helpmessage = config["General"]["helpmessage"]
+        logging.info("'{guild_name}'\t{member}\t{action}".format(guild_name=guild.name, member=user, action="request_help"))
+        helpmessage = generate_help()
         await user.send(helpmessage.format(user=user))
+        return
     
     #Games
     if message.content == '!games':
+        logging.info("'{guild_name}'\t{member}\t{action}".format(guild_name=guild.name, member=user, action="games_list"))
         games_list = games.get_channel_list()
-        await user.send("Here is the current game list:\n" + games_list)
+        await channel.send("{user}, here is the current game list:\n{games_list}".format(user=user.mention, games_list=games_list))
+        return
     
     #Join a game role
     if message.content.startswith("!join game"):
         game_name = message.content[11:]
-        print("'{user}' has requested to join game channel for '{game_name}'".format(user=user.name, game_name=game_name))
-        member = await userToMember(user)
+        logging.info("'{guild_name}'\t{member}\t{action}\t'{game_name}'".format(guild_name=guild.name, member=user, action="join_game", game_name=game_name))
         role = games.get_role(game_name)
-
-        #Is the user a member of the server?
-        if member is None:
-            print("User membership not found.")
-            await user.send("We could find your membership to a server!")
-            return
 
         #Does the role exist?
         if role is None:
-            print("Game not found.")
-            await user.send("We could find a matching game!")
+            logging.info("'{guild_name}'\t{member}\t{action}\t'{game_name}\t{result}'".format(guild_name=guild.name, member=user, action="join_game", game_name=game_name,result="no_matching_game"))
+            await channel.send("{user}, we couldn't find a matching game for '{game_name}'.".format(user=user.mention, game_name=game_name))
             return
 
         #Is the member already in the role?
-        has_role = await does_member_have_role(member, role)
+        has_role = await does_member_have_role(guild, user, role)
         if has_role:
-            print("User already has role.")
-            await user.send("You are already a member of {game_name}".format(game_name=game_name))
+            logging.info("'{guild_name}'\t{member}\t{action}\t'{game_name}\t{result}'".format(guild_name=guild.name, member=user, action="join_game", game_name=game_name,result="has_role"))
+            await channel.send("{user}, you are already a member of {game_name}".format(user=user.mention, game_name=game_name))
             return
         
-        await join_role(member, role)
-        await user.send("You have been added to {game_name}!".format(game_name=game_name))
+        await join_role(guild, user, role)
+        logging.info("'{guild_name}'\t{member}\t{action}\t'{game_name}\t{result}'".format(guild_name=guild.name, member=user, action="join_game", game_name=game_name, result="added"))
+        await channel.send("{user}, you have been added to {game_name}".format(user=user.mention, game_name=game_name))
+        return
     
     #Leave a game role
     if message.content.startswith("!leave game"):
         game_name = message.content[12:]
-        print("'{user}' has requested to leave game channel for '{game_name}'".format(user=user.name, game_name=game_name))
-        member = await userToMember(user)
+        logging.info("'{guild_name}'\t{member}\t{action}\t'{game_name}'".format(guild_name=guild.name, member=user, action="leave_game", game_name=game_name))
         role = games.get_role(game_name)
-
-        #Is the user a member of the server?
-        if member is None:
-            print("User membership not found.")
-            await user.send("We could find your membership to a server!")
-            return
 
         #Does the role exist?
         if role is None:
-            print("Game not found.")
-            await user.send("We could find a matching game!")
+            logging.info("'{guild_name}'\t{member}\t{action}\t'{game_name}\t{result}'".format(guild_name=guild.name, member=user, action="leave_game", game_name=game_name,result="no_matching_game"))
+            await channel.send("{user}, we couldn't find a matching game for '{game_name}'.".format(user=user.mention, game_name=game_name))
             return
 
         #Is the member part of the role?
-        has_role = await does_member_have_role(member, role)
+        has_role = await does_member_have_role(guild, user, role)
         if not has_role:
-            print("User doesn't have role.")
-            await user.send("You aren't a member of {game_name}".format(game_name=game_name))
+            logging.info("'{guild_name}'\t{member}\t{action}\t'{game_name}\t{result}'".format(guild_name=guild.name, member=user, action="leave_game", game_name=game_name,result="doesnt_have_role"))
+            await channel.send("{user}, you aren't a member of {game_name}".format(user=user.mention, game_name=game_name))
             return
 
-        await leave_role(member, role)
-        await user.send("You have been removed from {game_name}!".format(game_name=game_name))
+        await leave_role(guild, user, role)
+        logging.info("'{guild_name}'\t{member}\t{action}\t'{game_name}\t{result}'".format(guild_name=guild.name, member=user, action="leave_game", game_name=game_name, result="removed"))
+        await channel.send("{user}, you have been removed from {game_name}".format(user=user.mention, game_name=game_name))
+        return
 
-print("Starting Discord Bot")
-client.run(config["General"]["secret"])
+logging.info("Starting Discord Bot.")
+client.run(secret)
--- media/media.py
+++ media/media.py
@@ -1,5 +1,8 @@
+import logging
+
 class Media:
-    def __init__(self, media_type, category_name):
+    def __init__(self, guild, media_type, category_name):
+        self.guild = guild
         self.media_type = media_type
         self.category_name = category_name
         self.category_id = 0
@@ -9,21 +12,21 @@ class Media:
         self.approver_role_name = ""
 
     def set_category_id(self, id):
-        print("We are setting the category id for '{media_type}' to '{id}'".format(media_type=self.media_type, id=id))
+        logging.info("'{guild_name}'\t{action}\t{media_type}\t{cat_id}".format(guild_name=self.guild.name, action="set_cat_id", media_type=self.media_type, cat_id=id))
         self.category_id = id
 
-    def get_channels(self, guild):
-        print("Getting all channels under '{guild_name}-{cat_name}'.".format(guild_name=guild.name, cat_name=self.category_name))
+    def get_channels(self):
+        logging.info("'{guild_name}'\t{action}\t{media_type}".format(guild_name=self.guild.name, action="get_channels", media_type=self.media_type))
 
         if self.category_id == 0:
-            print("ERROR: The category ID has not been set for {role_type}!")
+            logging.error("'{guild_name}'\t{action}\t{media_type}\t{result}".format(guild_name=self.guild.name, action="get_channels", media_type=self.media_type, result="Category ID not set"))
             return
         
-        for channel in guild.channels:
+        for channel in self.guild.channels:
             if channel.category_id == self.category_id:
                 self.channels[channel.name] = channel
         
-        print("Channels for '{guild_name}-{cat_name}' have been updated to the following:\n{channels}".format(guild_name=guild.name, cat_name=self.category_name, channels=self.channels))
+        logging.info("'{guild_name}'\t{action}\t{media_type}\t{result}".format(guild_name=self.guild.name, action="get_channels", media_type=self.media_type, result=self.channels))
 
     def get_channel_list(self):
         val = ""
@@ -34,66 +37,52 @@ class Media:
         return val
     
     def get_role(self, media_name):
-        print("Attempting to find a matching role for '{role_type}-{media_name}'".format(role_type=self.media_type, media_name=media_name))
-
-        role = None
+        logging.info("'{guild_name}'\t{action}\t{media_type}\t'{media_name}'".format(guild_name=self.guild.name, action="get_role", media_type=self.media_type, media_name=media_name))
 
         if media_name in self.roles:
             role = self.roles[media_name]
-            print("Found a matching role '{role}'".format(role=role))
         else:
-            print("No matching role found.")
+            role = None
 
+        logging.info("'{guild_name}'\t{action}\t{media_type}\t'{media_name}'\t'{result}'".format(guild_name=self.guild.name, action="get_role", media_type=self.media_type, media_name=media_name, result=role))
         return role
 
-    def get_roles(self, guild):
-        print("Getting all roles under '{guild_name}-{cat_name}'.".format(guild_name=guild.name, cat_name=self.media_type))
+    def get_roles(self):
+        logging.info("'{guild_name}'\t{action}\t{media_type}".format(guild_name=self.guild.name, action="get_roles", media_type=self.media_type))
 
         prefix = self.media_type + "-"
         prefix_len = len(prefix)
 
-        for role in guild.roles:
+        for role in self.guild.roles:
             if role.name.startswith(prefix):
                 media_name = role.name[prefix_len:]
                 self.roles[media_name] = role
         
-        print("We have finished getting the roles under '{guild_name}-{cat_name}'.\n{roles}".format(guild_name=guild.name, cat_name=self.media_type,roles=self.roles))
+        logging.info("'{guild_name}'\t{action}\t{media_type}\t{result}".format(guild_name=self.guild.name, action="get_roles", media_type=self.media_type, result=self.roles))
     
-    def create_role(self, guild, media_name):
-        print("Request to create role for '{guild_name}-{media_type}-{media_name}".format(guild_name=guild.name, media_type=self.media_type, media_name=media_name))
+    async def create_role(self, media_name):
+        logging.info("'{guild_name}'\t{action}\t{media_type}\t'{media_name}'".format(guild_name=self.guild.name, action="create_role", media_type=self.media_type, media_name=media_name))
 
         role_name = "{media_type}-{media_name}".format(media_type=self.media_type, media_name=media_name)
 
+        #TODO: Add Try\Catch Block
         #Create the role in the guild
-        new_role = guild.create_role(name=role_name)
+        new_role = await self.guild.create_role(name=role_name)
 
         #Add the new role to our DB
         self.roles[media_name] = new_role
 
-        print("Created new role:\n{role}".format(role=new_role))
+        logging.info("'{guild_name}'\t{action}\t{media_type}\t'{media_name}'\t{result}".format(guild_name=self.guild.name, action="create_role", media_type=self.media_type, media_name=media_name, result=new_role))
         return new_role
     
-    def create_missing_roles(self, guild):
-        print("Request to create missing roles for '{guild_name}-{media_type}'".format(guild_name=guild.name, media_type=self.media_type))
+    async def create_missing_roles(self):
+        logging.info("'{guild_name}'\t{action}\t{media_type}\t".format(guild_name=self.guild.name, action="create_missing_roles", media_type=self.media_type))
         
         for media_name in self.channels:
             #Check if the channel name has a role
             if media_name not in self.roles:
-                print("We found a missing role! We are going to create a role! '{guild_name}-{media_type}-{media_name}'".format(guild_name=guild.name, media_type=self.media_type, media_name=media_name))
-
-                new_role = self.create_role(guild, media_name)
+                new_role = await self.create_role(media_name)
 
                 #Set Permissions on role
-                self.channels[media_name].set_permissions(new_role, read_messages=True, send_messages=True, read_message_history=True)
-
-    async def join_role(self, member, role):
-        print("We are adding {member} to '{role}'.".format(member=member, role=role))
-
-        await member.add_roles(role)
-        print("Added {member} to '{role}'".format(member=member.name, role=role))
-    
-    async def leave_role(self, member, role):
-        print("We are removing {member} from '{role}'.".format(member=member, role=role))
-
-        await member.remove_roles(role)
-        print("Removed {member} from '{role}'".format(member=member.name, role=role))
+                await self.channels[media_name].set_permissions(new_role, read_messages=True, send_messages=True, read_message_history=True)
+                logging.info("'{guild_name}'\t{action}\t{media_type}\t{media_name}\t{result}".format(guild_name=self.guild.name, action="create_missing_roles", media_type=self.media_type, media_name=media_name, result=new_role))
